c1ab705baa4459c40d9a8a9492b619ecb9055bf0
Guilherme Somenzi <guilherme@somenzi.me>
2024-01-12 19:03:59 -0300

    remove contexto duplicado

--- src/app/app/transactions/components/TransactionsListItem/index.tsx
+++ src/app/app/transactions/components/TransactionsListItem/index.tsx
@@ -8,7 +8,7 @@ import { useTransaction } from '../../providers/TransactionProvider';
 import { motion } from 'framer-motion';
 import TransactionListItemDescription from './components/TransactionListItemDescription';
 import TransactionListItemContent from './components/TransactionListItemContent';
-import TransactionDetailsProvider, { useTransactionDetails } from './providers/TransactionDetailsProvider';
+import { useTransactionDetails } from './providers/TransactionDetailsProvider';
 
 const contextMenuVariants = {
     hidden: { opacity: 0, scale: 0, width: 0 },
@@ -38,57 +38,55 @@ export default function TransactionsListItem(props: TransactionsListItemProps) {
 
     return (
         <>
-            <TransactionDetailsProvider transaction={transaction}>
-                <List.Item
-                    onMouseEnter={() => setShowContext(true)}
-                    onMouseLeave={() => setShowContext(false)}
-                    style={{ cursor: 'pointer' }}
-                    onClick={() =>
-                        setSelectedTransaction({
-                            transaction: transaction,
-                            action: 'details',
-                        })
-                    }>
-                    {!isGrouped && (
-                        <motion.div
-                            layout
-                            variants={contextMenuVariants}
-                            initial="hidden"
-                            animate={showContext || selectedTransactions.includes(transaction) ? 'visible' : 'hidden'}
-                            exit="hidden">
-                            <Checkbox
-                                style={{ marginRight: '1rem' }}
-                                onChange={(e) => handleItemSelection(e, e.target.checked)}
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    e.stopPropagation();
-                                }}
-                            />
-                        </motion.div>
-                    )}
-                    <List.Item.Meta
-                        style={{ position: 'relative' }}
-                        title={isGrouped ? group?.name : transaction.description}
-                        description={<TransactionListItemDescription handleShowGroupItems={handleShowGroupItems} />}
-                    />
-                    <TransactionListItemContent />
-                </List.Item>
-                {isGrouped && showGroupItems && (
-                    <List>
-                        {group?.transactions?.map((t) => (
-                            <List.Item key={t.id}>
-                                <List.Item.Meta description={t.description} />
-                                <Typography.Text type="secondary">
-                                    {Intl.NumberFormat('pt-BR', {
-                                        style: 'currency',
-                                        currency: 'BRL',
-                                    }).format(Number(t.value))}
-                                </Typography.Text>
-                            </List.Item>
-                        ))}
-                    </List>
+            <List.Item
+                onMouseEnter={() => setShowContext(true)}
+                onMouseLeave={() => setShowContext(false)}
+                style={{ cursor: 'pointer' }}
+                onClick={() =>
+                    setSelectedTransaction({
+                        transaction: transaction,
+                        action: 'details',
+                    })
+                }>
+                {!isGrouped && (
+                    <motion.div
+                        layout
+                        variants={contextMenuVariants}
+                        initial="hidden"
+                        animate={showContext || selectedTransactions.includes(transaction) ? 'visible' : 'hidden'}
+                        exit="hidden">
+                        <Checkbox
+                            style={{ marginRight: '1rem' }}
+                            onChange={(e) => handleItemSelection(e, e.target.checked)}
+                            onClick={(e) => {
+                                e.preventDefault();
+                                e.stopPropagation();
+                            }}
+                        />
+                    </motion.div>
                 )}
-            </TransactionDetailsProvider>
+                <List.Item.Meta
+                    style={{ position: 'relative' }}
+                    title={isGrouped ? group?.name : transaction.description}
+                    description={<TransactionListItemDescription handleShowGroupItems={handleShowGroupItems} />}
+                />
+                <TransactionListItemContent />
+            </List.Item>
+            {isGrouped && showGroupItems && (
+                <List>
+                    {group?.transactions?.map((t) => (
+                        <List.Item key={t.id}>
+                            <List.Item.Meta description={t.description} />
+                            <Typography.Text type="secondary">
+                                {Intl.NumberFormat('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL',
+                                }).format(Number(t.value))}
+                            </Typography.Text>
+                        </List.Item>
+                    ))}
+                </List>
+            )}
         </>
     );
 }
